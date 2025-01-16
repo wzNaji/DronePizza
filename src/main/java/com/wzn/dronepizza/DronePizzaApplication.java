9c757aee0e4c2d555ab192fc0edf0250ad43d486
wzNaji <walid.z.naji@gmail.com>
2025-01-16 09:33:11 +0100

    initial

---
+++ src/main/java/com/wzn/dronepizza/DronePizzaApplication.java
@@ -0,0 +1,13 @@
+package com.wzn.dronepizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DronePizzaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DronePizzaApplication.class, args);
+    }
+
+}
